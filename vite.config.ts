a07f14ea6ccf96c8cefa97936c45c35f0b2ba452
pelinkoz <139854016+pelinkoz@users.noreply.github.com>
2025-11-20 15:58:17 +0300

    update main branch

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/My_portfolio/',  
-  plugins: [
-    react(),
-    
-  ],
+  plugins: [react(),],
 })
